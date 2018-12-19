e99ccdff66369738bcdb97adfd867b21de8e6098
Kyle Hinze <kylehinze@Kyles-MacBook-Pro.local>
2018-12-19 14:08:19 -0800

    Watch video instead

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <h1>It's dangerous to go alone. Take this.</h1>
+    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Watch a video</Link>
+  </Layout>
+)
+
+export default IndexPage
